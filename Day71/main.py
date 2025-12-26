79bb332e3d35ade224191dd27606804db6a55cda
Timskid11 <timilehin.oyinlola240591187@st.lau.edu.ng>
2025-12-26 22:58:03 +0100

    add Day71-82

--- Day71/main.py
+++ Day71/main.py
@@ -1,8 +1,9 @@
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask.cli import load_dotenv
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-
+import os
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
@@ -12,7 +13,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
-
+from dotenv import load_dotenv
+load_dotenv()
 
 '''
 Make sure the required packages are installed: 
@@ -29,7 +31,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -48,7 +50,10 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    "DB_URI", "sqlite:///posts.db"
+)
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -291,4 +296,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)
